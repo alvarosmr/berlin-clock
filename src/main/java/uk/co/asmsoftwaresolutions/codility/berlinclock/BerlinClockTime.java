fd0c545c1902ef7b9e35a9711dd9826a18aa833d
alvarosmr <alvaro@asmsoftwaresolutions.co.uk>
2018-06-19 21:37:16 +0100

    Overridden toString, equals and hashCode methods in BerlinClockTime

--- src/main/java/uk/co/asmsoftwaresolutions/codility/berlinclock/BerlinClockTime.java
+++ src/main/java/uk/co/asmsoftwaresolutions/codility/berlinclock/BerlinClockTime.java
@@ -1,7 +1,14 @@
 package uk.co.asmsoftwaresolutions.codility.berlinclock;
 
-import java.time.LocalTime;
+import com.google.common.base.Strings;
 
+import java.time.LocalTime;
+import java.util.Objects;
+
+/**
+ * A Berlin clock time immutable object representing the time as described on
+ * https://en.wikipedia.org/wiki/Mengenlehreuhr.
+ */
 public final class BerlinClockTime {
 
     private final byte seconds;
@@ -24,11 +31,19 @@ public final class BerlinClockTime {
         this.lampsOnSingleMinutesRow = (byte) lampsOnSingleMinutesRow;
     }
 
+    /**
+     * Static factory method to return a BerlinClockTime whose time is equivalent to that of the specified
+     * {@code localTime}.
+     *
+     * Note that zero hours in the LocalTime is represented by all the lamps on in the hours rows of the Berlin Clock.
+     *
+     * @param localTime time of the Berlin clock time to return
+     * @return a BerlinClockTime with the specified time
+     */
     public static BerlinClockTime valueOf(LocalTime localTime) {
         int hours = localTime.getHour();
         int minutes = localTime.getMinute();
 
-        // Zero hours in LocalTime is represented by all the lamps on in the hours rows of the Berlin Clock
         int lampsOnFiveHoursRow = hours == 0 ? 4 : hours / 5;
         int lampsOnSingleHoursRow = hours == 0 ? 4 : hours % 5;
         int lampsOnFiveMinutesRow = minutes / 5;
@@ -60,5 +75,75 @@ public final class BerlinClockTime {
 
     public int getLampsOnSingleMinutesRow() {
         return lampsOnSingleMinutesRow;
+    }
+
+    /**
+     * Returns the string representation of this Berlin clock time. The string consists of 28 characters whose format
+     * is "s HHHH hhhh MMMMMMMMMMM mmmm", where ('O' -> Off, 'Y' -> Yellow light, 'R' -> Red light):
+     *
+     *   - 's' represents the seconds lamp (if 'O' the lamp is off, if 'Y' the lamp is on)
+     *   - 'HHHH' represents the lamps for the five hours row (if 'O' the lamp is off, if 'R' the lamp is on)
+     *   - 'hhhh' represents the lamps for the single hours row (if 'O' the lamp is off, if 'R' the lamp is on)
+     *   - 'MMMMMMMMMMM' represents the lamps for the five minutes row (if 'O' the lamp is off, if 'Y' or 'R' the lamp
+     *      is on, 'R' is the color for 15, 30 and 45 minutes lamps)
+     *   - 'mmmm' represents the lamps for the single minutes row (if 'O' the lamp is off, if 'Y' the lamp is on)
+     *
+     * Examples:
+     *
+     *   - Time 15:30:01 -> "O RRRO OOOO YYRYYROOOOO OOOO"
+     *   - Time 23:59:59 -> "O RRRR RRRO YYRYYRYYRYY YYYY"
+     *   - Time 00:00:00 -> "Y RRRR RRRR OOOOOOOOOOO OOOO"
+     *   - Time 00:00:01 -> "O RRRR RRRR OOOOOOOOOOO OOOO"
+     *   - Time 00:59:59 -> "O RRRR RRRR YYRYYRYYRYY YYYY"
+     *   - Time 01:00:00 -> "Y OOOO ROOO OOOOOOOOOOO OOOO"
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(28)
+                .append(seconds % 2 == 0 ? 'Y' : 'O')
+                .append(' ')
+                .append(Strings.repeat("R", lampsOnFiveHoursRow))
+                .append(Strings.repeat("O", 4 - lampsOnFiveHoursRow))
+                .append(' ')
+                .append(Strings.repeat("R", lampsOnSingleHoursRow))
+                .append(Strings.repeat("O", 4 - lampsOnSingleHoursRow))
+                .append(' ');
+
+        for (int i = 0; i < lampsOnFiveMinutesRow; i++) {
+            // 15, 30 or 45 minutes
+            if (i == 2 || i == 5 || i == 8)
+                builder.append('R');
+            else
+                builder.append('Y');
+        }
+
+        builder.append(Strings.repeat("O", 11 - lampsOnFiveMinutesRow))
+                .append(' ')
+                .append(Strings.repeat("Y", lampsOnSingleMinutesRow))
+                .append(Strings.repeat("O", 4 - lampsOnSingleMinutesRow));
+
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        BerlinClockTime that = (BerlinClockTime) o;
+        return seconds == that.seconds
+                && lampsOnFiveHoursRow == that.lampsOnFiveHoursRow
+                && lampsOnSingleHoursRow == that.lampsOnSingleHoursRow
+                && lampsOnFiveMinutesRow == that.lampsOnFiveMinutesRow
+                && lampsOnSingleMinutesRow == that.lampsOnSingleMinutesRow;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                seconds, lampsOnFiveHoursRow, lampsOnSingleHoursRow, lampsOnFiveMinutesRow, lampsOnSingleMinutesRow);
     }
 }
--- src/test/java/uk/co/asmsoftwaresolutions/codility/berlinclock/BerlinClockTimeTest.java
+++ src/test/java/uk/co/asmsoftwaresolutions/codility/berlinclock/BerlinClockTimeTest.java
@@ -20,11 +20,12 @@ public class BerlinClockTimeTest {
             "40, 0", "41, 1", "42, 2", "43, 3", "44, 4", "45, 0", "46, 1", "47, 2", "48, 3", "49, 4",
             "50, 0", "51, 1", "52, 2", "53, 3", "54, 4", "55, 0", "56, 1", "57, 2", "58, 3", "59, 4"
     })
-    void when_TimeIsGiven_expect_CorrectLampsOnSingleMinutesRow(int minutes, int lampsOnSingleMinutesRow) {
+    void when_TimeIsGiven_expect_CorrectLampsOnSingleMinutesRow(int minutes, int expectedLampsOn) {
         BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.of(0, Integer.valueOf(minutes), 0));
 
-        assertEquals(lampsOnSingleMinutesRow, result.getLampsOnSingleMinutesRow(),
-                "Wrong number of lamps on in the single minutes row");
+        assertEquals(expectedLampsOn, result.getLampsOnSingleMinutesRow(),
+                "Wrong number of lamps on in the single minutes row (expected " + expectedLampsOn + " for " + minutes
+                        + " minutes)");
     }
 
     @DisplayName("Testing five minutes row when factory method valueOf is called")
@@ -37,11 +38,12 @@ public class BerlinClockTimeTest {
             "40, 8", "41, 8", "42, 8", "43, 8", "44, 8", "45, 9", "46, 9", "47, 9", "48, 9", "49, 9",
             "50, 10", "51, 10", "52, 10", "53, 10", "54, 10", "55, 11", "56, 11", "57, 11", "58, 11", "59, 11"
     })
-    void when_TimeIsGiven_expect_CorrectLampsOnFiveMinutesRow(int minutes, int lampsOnFiveMinutesRow) {
+    void when_TimeIsGiven_expect_CorrectLampsOnFiveMinutesRow(int minutes, int expectedLampsOn) {
         BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.of(0, Integer.valueOf(minutes), 0));
 
-        assertEquals(lampsOnFiveMinutesRow, result.getLampsOnFiveMinutesRow(),
-                "Wrong number of lamps on in the five minutes row");
+        assertEquals(expectedLampsOn, result.getLampsOnFiveMinutesRow(),
+                "Wrong number of lamps on in the five minutes row (expected " + expectedLampsOn + " for " + minutes
+                        + " minutes)");
     }
 
     @DisplayName("Testing single hours row when factory method valueOf is called")
@@ -50,11 +52,12 @@ public class BerlinClockTimeTest {
             "0, 4", "1, 1", "2, 2", "3, 3", "4, 4", "5, 0", "6, 1", "7, 2", "8, 3", "9, 4", "10, 0", "11, 1", "12, 2",
             "13, 3", "14, 4", "15, 0", "16, 1", "17, 2", "18, 3", "19, 4", "20, 0", "21, 1", "22, 2", "23, 3"
     })
-    void when_TimeIsGiven_expect_CorrectLampsOnSingleHoursRow(int hours, int lampsOnSingleHoursRow) {
+    void when_TimeIsGiven_expect_CorrectLampsOnSingleHoursRow(int hours, int expectedLampsOn) {
         BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.of(Integer.valueOf(hours), 0, 0));
 
-        assertEquals(lampsOnSingleHoursRow, result.getLampsOnSingleHoursRow(),
-                "Wrong number of lamps on in the single hours row");
+        assertEquals(expectedLampsOn, result.getLampsOnSingleHoursRow(),
+                "Wrong number of lamps on in the single hours row (expected " + expectedLampsOn + " for " + hours
+                        + " hours)");
     }
 
     @DisplayName("Testing five hours row when factory method valueOf is called")
@@ -63,11 +66,12 @@ public class BerlinClockTimeTest {
             "0, 4", "1, 0", "2, 0", "3, 0", "4, 0", "5, 1", "6, 1", "7, 1", "8, 1", "9, 1", "10, 2", "11, 2", "12, 2",
             "13, 2", "14, 2", "15, 3", "16, 3", "17, 3", "18, 3", "19, 3", "20, 4", "21, 4", "22, 4", "23, 4"
     })
-    void when_TimeIsGiven_expect_CorrectLampsOnFiveHoursRow(int hours, int lampsOnFiveHoursRow) {
+    void when_TimeIsGiven_expect_CorrectLampsOnFiveHoursRow(int hours, int expectedLampsOn) {
         BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.of(Integer.valueOf(hours), 0, 0));
 
-        assertEquals(lampsOnFiveHoursRow, result.getLampsOnFiveHoursRow(),
-                "Wrong number of lamps on in the five hours row");
+        assertEquals(expectedLampsOn, result.getLampsOnFiveHoursRow(),
+                "Wrong number of lamps on in the five hours row (expected " + expectedLampsOn + " for " + hours
+                        + " hours)");
     }
 
     @DisplayName("Testing seconds when factory method valueOf is called")
@@ -84,5 +88,22 @@ public class BerlinClockTimeTest {
         BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.of(0, 0, Integer.valueOf(seconds)));
 
         assertEquals(expectedSeconds, result.getSeconds(), "Wrong number of seconds");
+    }
+
+    @DisplayName("Testing string representation of Berlin clock time")
+    @ParameterizedTest(name = "String representation of \"{0}\" should be \"{1}\"")
+    @CsvSource({
+            "15:30:01, O RRRO OOOO YYRYYROOOOO OOOO",
+            "23:59:59, O RRRR RRRO YYRYYRYYRYY YYYY",
+            "00:00:00, Y RRRR RRRR OOOOOOOOOOO OOOO",
+            "00:00:01, O RRRR RRRR OOOOOOOOOOO OOOO",
+            "00:59:59, O RRRR RRRR YYRYYRYYRYY YYYY",
+            "01:00:00, Y OOOO ROOO OOOOOOOOOOO OOOO"
+    })
+    void when_toStringIsCalled_expect_CorrectStringRepresentation(String localTime, String expectedString) {
+        BerlinClockTime result = BerlinClockTime.valueOf(LocalTime.parse(localTime));
+
+        assertEquals(expectedString, result.toString(), "Wrong Berlin clock time string representation for the time "
+                + localTime);
     }
 }
